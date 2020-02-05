08fc7110296f07618a053e35f764f338fa35bbf9
Alec Kirkley <alec.w.kirkley@gmail.com>
2020-02-05 13:05:00 -0500

    Added pipeline for running ABC simulations with different models/distances/priors

---
+++ ABC_code/.ipynb_checkpoints/distances-checkpoint.py
@@ -0,0 +1,64 @@
+class distance:
+    
+    """
+    distance class
+    args: 
+        -reference graph 'G0' (igraph object)
+        -proposal graph 'G_prop' generated from model (igraph object)
+        -function 'measure' taking two igraph objects and returning a scalar 
+        -'**measure_kwargs' are optional keyword args that may apply to 'measure'
+    """
+    
+    def __init__(self,G0,measure,**measure_kwargs):
+        
+        """
+        distance class attributes
+        """
+        
+        self.measure = measure
+        self.G0 = G0
+        self.measure_kwargs = measure_kwargs
+        
+    def evaluate_unnormalized(self,G_prop):
+        
+        """
+        evaluate unnormalized distance between G0 and igraph object G_prop
+        """
+        self.G_prop = G_prop
+        
+        return self.measure(self.G0,self.G_prop,**self.measure_kwargs)
+    
+    def evaluate_normalized(self,G_prop,num_nulls,null_generator,**null_kwargs): 
+        
+        """
+        evaluate normalized distance between G0 and igraph object G_prop
+        args:
+            -'num_nulls' is number of null graphs to generate to compute z-score
+            -'null_generator' is function that takes in a reference graph G0 and outputs an igraph object that is randomized
+                    based on some property of G0 and some model/parameters included in **null_kwargs
+        """
+        
+        null_distance_vals = [self.measure(self.G0,null_generator(self.G0,**null_kwargs),**self.measure_kwargs)\
+                              for _ in range(num_nulls)]
+        null_mean,null_std = np.mean(null_distance_vals),np.std(null_distance_vals)
+        
+        return (self.evaluate_unnormalized(G_prop) - null_mean)/null_std
+    
+
+    
+"""
+
+Custom distance functions go here
+
+args:
+    -G0: reference graph in igraph format
+    -G_prop: proposed graph in igraph format
+    -any other args should be named and set to some default value (i.e. beta=1 in the first example)
+
+"""    
+
+def distance_KS_degree(G0,G_prop,beta=1.):
+    """
+    Kolmogorov-Smirnov distance between distributions of degrees to the 'beta' power
+    """
+    return ss.ks_2samp(np.array(G0.vs.degree())**beta,np.array(G_prop.vs.degree())**beta).statistic
---
+++ ABC_code/ABC.py
@@ -0,0 +1,126 @@
+class ABC:
+    """
+    ABC simulator class
+    args:
+        -'acc': desired acceptance rate
+        -'model_object': model object
+        -'distance_object': distance object
+        -'G0': reference graph to fit for posterior
+        -'eps': numerical cutoff value (if applicable)
+            -uses acceptance rate if eps == None (default)
+        -'hamiltonian': hamiltonian function
+            -h(d,eps) is a function of distance d and cutoff scale eps
+            -uses hard thresholding hamiltonian by default
+    """
+    
+    def __init__(self,acc,model_object,distance_object,eps='None',hamiltonian='None',**hamiltonian_kwargs):
+        self.acc = acc
+        self.model = model_object
+        self.distance = distance_object
+        self.G0 = self.distance.G0
+        self.eps = eps
+        self.hamiltonian = hamiltonian
+        self.hamiltonian_kwargs = hamiltonian_kwargs
+        
+        self.posterior = dict.fromkeys(self.model.free_param_names)
+        self.acceptance_rate = 'None'
+        
+    def get_eps_from_acc(self,max_runs='None',tol='None',eps_lower_init='None',eps_upper_init='None',\
+                         window_size='None',num_sims='None'):
+        """
+        finds epsilon value that gives approximately the acceptance rate 'acc'
+            for the given distance object
+        stores this as self.eps
+        takes moving average of acceptance rate and tries to get within 'tol' of 'acc'
+        updates guess for epsilon by repeatedly bisecting an interval
+        args:
+            -'eps_init': initial guess for epsilon
+            -'max_runs': number of runs algorithm allowed to run for if acc_current
+                    never within 'tol' of 'acc'
+            -'tol': tolerance of error for accepting acc_current
+            -'eps_lower/upper_init': lower/upper cutoff for epsilon bisection
+        """
+        
+        
+        eps_currents = []
+        for _ in range(num_sims):
+            acc_current,run_count = 100,0
+            eps_upper = eps_upper_init
+            eps_lower = eps_lower_init
+            proposal_results = [False]*window_size
+            while (run_count < max_runs):
+
+                eps_current = np.random.rand()*(eps_upper-eps_lower) + eps_lower
+
+                for w in range(window_size):
+
+                    G_prop = self.model.generate_graph()
+                    params_prop = self.model.free_parameters
+
+                    dist = self.distance.evaluate_unnormalized(G_prop)
+
+                    if np.log(np.random.rand()) < -self.hamiltonian(dist,eps_current,**hamiltonian_kwargs):
+                        proposal_results[w] = True
+                    else:
+                        proposal_results[w] = False
+
+                acc_current = Counter(proposal_results)[True]/window_size
+
+                if abs(acc_current-self.acc) > tol:
+                    if acc_current < self.acc:
+                        eps_lower = eps_current 
+                    else:
+                        eps_upper = eps_current
+                    eps_current = (eps_upper + eps_lower)/2
+
+                else:
+                    break
+
+                run_count += 1
+            eps_currents.append(eps_current)
+            
+        self.eps = np.mean(eps_currents) 
+        
+    def run_sims(self,n_runs='None'):
+        """
+        runs ABC with soft thresholding (hard thresholding imposed by the default hamiltonian h_hard_cutoff)
+        n_runs is desired number of runs (proposals) for the simulation
+        sets self.accepted_params and self.acceptance_rate
+        """
+        
+        accepted_params = []
+        proposal_results = []
+        run_count = 0
+        while (run_count < n_runs):
+                
+            G_prop = self.model.generate_graph()
+            params_prop = copy.deepcopy(self.model.free_parameters)
+
+            dist = self.distance.evaluate_unnormalized(G_prop)
+
+            if np.log(np.random.rand()) < -self.hamiltonian(dist,self.eps,**hamiltonian_kwargs):
+                proposal_results.append(True)
+                accepted_params.append(params_prop)
+            else:
+                proposal_results.append(False) 
+                    
+            run_count += 1 
+        
+        params = self.model.free_param_names
+        posterior_dict = dict.fromkeys(params)
+        for p in params:
+            posterior_dict[p] = [res[p] for res in accepted_params]
+        self.posterior = posterior_dict
+        self.acceptance_rate = Counter(proposal_results)[True]/n_runs
+    
+    def save_to_file(self,filepath,**save_kwargs):
+        """
+        write to csv with column names as parameter names and column values as the 
+            sampled values of that parameter
+        """
+        try:
+            df = pd.DataFrame.from_dict(posterior_dict)
+            df.to_csv(filepath,**save_kwargs)
+        except:
+            print('No posterior data to write!')
+        
